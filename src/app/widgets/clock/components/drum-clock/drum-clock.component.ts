6ce140b7144e689c87c0665fb3e5c9eb64c00051
Elizaveta.Shvedchikova <Elizaveta.Shvedchiko@tatar.ru>
2023-11-30 16:33:05 +0300

    save

--- src/app/widgets/clock/components/drum-clock/drum-clock.component.ts
+++ src/app/widgets/clock/components/drum-clock/drum-clock.component.ts
@@ -1,6 +1,19 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { TimeService } from 'app/shared/services/time.service';
+import { getRange } from 'app/shared/functions/getRange';
+import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
+import { BehaviorSubject } from 'rxjs';
 
+interface TimeDrum {
+  hoursFirst: number;
+  hoursLast: number;
+  minutesFirst: number;
+  minutesLast: number;
+  secondsFirst: number;
+  secondsLast: number;
+}
+
+@UntilDestroy()
 @Component({
   selector: 'app-drum-clock',
   templateUrl: './drum-clock.component.html',
@@ -8,5 +21,25 @@ import { TimeService } from 'app/shared/services/time.service';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class DrumClockComponent {
-  constructor(readonly dateAndTimeService: TimeService) {}
+  private readonly _timeModel$ = new BehaviorSubject<TimeDrum | undefined>(undefined);
+  public readonly timeModel$ = this._timeModel$.asObservable();
+
+  constructor(readonly dateAndTimeService: TimeService) {
+    this.dateAndTimeService.time$.pipe(untilDestroyed(this)).subscribe((time) => {
+      this._timeModel$.next({
+        hoursFirst: Math.floor(time.Hr / 10),
+        hoursLast: time.Hr,
+        minutesFirst: Math.floor(time.Min / 10),
+        minutesLast: time.Min,
+        secondsFirst: Math.floor(time.Sec / 10),
+        secondsLast: time.Sec,
+      });
+    });
+  }
+
+  protected readonly getRange = getRange;
+
+  getTransformStyle(value: number) {
+    return `rotateY(90deg) rotate(${value * 36}deg)`;
+  }
 }
