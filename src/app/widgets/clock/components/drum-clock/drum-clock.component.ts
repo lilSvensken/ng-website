02c2179ecb6bc18aa46f561adff339c318f4eac6
Elizaveta.Shvedchikova <Elizaveta.Shvedchiko@tatar.ru>
2023-11-30 11:59:46 +0300

    часы фикс

--- src/app/widgets/clock/components/drum-clock/drum-clock.component.ts
+++ src/app/widgets/clock/components/drum-clock/drum-clock.component.ts
@@ -1,6 +1,5 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { TimeService } from 'app/shared/services/time.service';
-import { getRange } from 'app/shared/functions/getRange';
 
 @Component({
   selector: 'app-drum-clock',
@@ -10,16 +9,4 @@ import { getRange } from 'app/shared/functions/getRange';
 })
 export class DrumClockComponent {
   constructor(readonly dateAndTimeService: TimeService) {}
-
-  public getRange(start: number, end: number) {
-    return getRange(start, end);
-  }
-
-  public getTransformStyle(division: number) {
-    return `rotate(${division * 6}deg)`;
-  }
-
-  public getTransformHrStyle(hr: number, min: number) {
-    return this.getTransformStyle(hr * 5 + min / 12);
-  }
 }
